287c2bcb2f6811c76381f3b415311787c41516ea
Maria Luiza Bigati <mbigati@hotmail.com>
2025-07-19 17:00:03 -0300

    Desafio concluido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+//Implementado
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Nokia 3310", "111111111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("654321", "iPhone 13", "222222222222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+
+
+
